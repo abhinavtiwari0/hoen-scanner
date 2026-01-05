32bd9e0ff2bbaede73076df828b387409f92e361
abhinavtiwari0 <abhinavtiwari8127@gmail.com>
2026-01-06 04:03:29 +0530

    Add files via upload

--- src/main/java/com/skyscanner/HoenScannerApplication.java
+++ src/main/java/com/skyscanner/HoenScannerApplication.java
@@ -3,6 +3,11 @@ package com.skyscanner;
 import io.dropwizard.core.Application;
 import io.dropwizard.core.setup.Bootstrap;
 import io.dropwizard.core.setup.Environment;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class HoenScannerApplication extends Application<HoenScannerConfiguration> {
 
@@ -12,17 +17,42 @@ public class HoenScannerApplication extends Application<HoenScannerConfiguration
 
     @Override
     public String getName() {
-        return "hoen-scanner";
+        return "HoenScanner";
     }
 
     @Override
     public void initialize(final Bootstrap<HoenScannerConfiguration> bootstrap) {
-
+        // Application initialization
     }
 
     @Override
-    public void run(final HoenScannerConfiguration configuration, final Environment environment) {
+    public void run(final HoenScannerConfiguration configuration,
+                    final Environment environment) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
 
+        // Rental Cars load kar rahe hain
+        List<SearchResult> carResults = Arrays.asList(
+                mapper.readValue(
+                        getClass().getClassLoader().getResource("rental_cars.json"),
+                        SearchResult[].class
+                )
+        );
+
+        // Hotels load kar rahe hain
+        List<SearchResult> hotelResults = Arrays.asList(
+                mapper.readValue(
+                        getClass().getClassLoader().getResource("hotels.json"),
+                        SearchResult[].class
+                )
+        );
+
+        // Dono ko ek list mein daal rahe hain
+        List<SearchResult> searchResults = new ArrayList<>();
+        searchResults.addAll(carResults);
+        searchResults.addAll(hotelResults);
+
+        // Resource register kar rahe hain
+        final SearchResource resource = new SearchResource(searchResults);
+        environment.jersey().register(resource);
     }
-
 }
